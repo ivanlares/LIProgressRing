d518c91315c6e76d4a4a3c80776328e848990c75
ivan lares <ivanlares1@gmail.com>
2018-09-25 09:29:34 -0700

    Initial Commit

---
+++ LIProgressRing/LIProgressRing.h
@@ -0,0 +1,19 @@
+//
+//  LIProgressRing.h
+//  LIProgressRing
+//
+//  Created by ivan lares on 9/25/18.
+//  Copyright © 2018 ivan lares. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LIProgressRing.
+FOUNDATION_EXPORT double LIProgressRingVersionNumber;
+
+//! Project version string for LIProgressRing.
+FOUNDATION_EXPORT const unsigned char LIProgressRingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LIProgressRing/PublicHeader.h>
+
+
